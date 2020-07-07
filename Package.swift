edcaebf77ba7252ae7fa8df216e02a6b8a7be6e0
Dino Constantinou <dconstantinou360@gmail.com>
2020-07-07 11:56:18 +0100

    Moves to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,23 @@
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
   name: "Beethoven",
+  products: [
+    .library(name: "Beethoven", targets: ["Beethoven"])
+  ],
   dependencies: [
-    .Package(url: "https://github.com/vadymmarkov/Pitchy.git", majorVersion: 2),
-  ]
+    .package(url: "git@github.com:dconstantinou/Pitchy.git", .branch("develop")),
+  ],
+  targets: [
+    .target(
+      name: "Beethoven",
+      dependencies: [
+        "Pitchy"
+      ],
+      path: "Source"
+    )
+  ],
+  swiftLanguageVersions: [.v5]
 )
